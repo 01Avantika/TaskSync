faa0dffc741452ce3b8d542eca344ea208aa25cd
Shivansh <your.aarrppiittaa416@gmail.com>
2025-04-21 14:13:15 +0530

    ✅ Fixed export report functionality for tasks and users

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { protect, adminOnly } = require(". /middlewares/authMiddleware");
+const { exportTaskReport, exportUserReport } = require("../controllers/reportControllers");
 const router = express.Router();
 router.get("/export/tasks",protect,adminOnly, exportTaskReport);
-router.get("/export/users",protect,adminOnly, exportUsersReport);
+router.get("/export/users",protect,adminOnly, exportUserReport);
 module.exports = router;
 
 	
